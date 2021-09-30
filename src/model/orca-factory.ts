cda5cb14d68d653c918b1ef355d3b97d395ba5fb
0xab <0xalexbai@gmail.com>
2021-09-30 18:57:11 +0800

    Use robust implementation.

--- src/model/orca-factory.ts
+++ src/model/orca-factory.ts
@@ -2,11 +2,11 @@ import { Connection } from "@solana/web3.js";
 import { OrcaPoolConfig, OrcaPool, OrcaFarmConfig, OrcaFarm } from "..";
 import { orcaPoolConfigs, orcaFarmConfigs } from "../constants";
 import { OrcaFarmImpl } from "./orca/farm/orca-farm";
-import { OrcaPoolImpl } from "./orca/pool/orca-pool";
+import { OrcaRobustPoolImpl } from "./orca/pool/orca-robust-pool";
 
 export class OrcaFactory {
-  getPool(connection: Connection, config: OrcaPoolConfig): OrcaPool {
-    return new OrcaPoolImpl(connection, orcaPoolConfigs[config]);
+  getPool(connections: Array<Connection>, config: OrcaPoolConfig): OrcaPool {
+    return new OrcaRobustPoolImpl(connections, orcaPoolConfigs[config]);
   }
 
   getFarm(connection: Connection, config: OrcaFarmConfig): OrcaFarm {
